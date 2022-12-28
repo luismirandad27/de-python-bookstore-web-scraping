71d83edf4b0cb548d2655c26435ee7c9262ea278
Luis Miguel Miranda <lmirandad27@gmail.com>
2022-12-28 11:45:32 -0800

    20221228 Adding README.md and python file with comments

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# de-python-bookstore-web-scraping
+# Building a Web Scraper with Python and BeautifulSoup Library
+
+<img src="assets/webscraping_portrait.png"
+     alt="Markdown Monster icon" style="border-radius:5px"/>
+
+Hey folks! This week I decided to learn a little bit about Web Scraping. You can check my article about a brief introduction of Web Scraping and how we can do this with Python on my Medium's Article here:
+
+Link: 
+&nbsp;
+### **Project's Scenario:**
+This project is meant to create a web scraper with Python on fictional web site called "Book to Scrape". This website contains information about books per different categories and the idea of this project is to extract the information of each book available on the website.
+
+<img src="assets/webpage_example.png"
+     alt="Markdown Monster icon" style="border-radius:5px"/>
+
+After scraping the website, we can export the data into Json, csv, txt files or into a database. In this case, I stored it on a csv file.
+&nbsp;
+### **Requirements:**
+&nbsp;
+1- You need to have python (obviously)
+
+2- Install `BeautifulSoup`
+```bash
+pip3 install beautifulSoup4
+```
+3- On your Python code you need to import it
+```python
+import BeautifulSoup
+```
+&nbsp;
+### **Some common methods:**
+Let's supose that you want to scrap a website and you have a URL. The website has the following HTML code:
+```html
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <title>My Website</title>
+    </head>
+    <body>
+        <div id='main-content'>
+            <p>
+                Hello World! This is the list of movies I like:
+            </p>
+            <ul>
+                <li class='movie'>Avatar</li>
+                <li class='movie'>Avengers Endgame</li>
+                <li class='movie'>Midnight in Paris</li>
+                <li class='movie'>Akira</li>
+            </ul>
+        </div>
+    </body>
+</html>
+``` 
+
+In your Python file, you can do the following:
+```python
+import request
+import BeautifulSoup
+
+url = [YOUR URL]
+page = requests.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
+```
+
+Your `soup` variable will hold the entire HTML code. The idea is to "move" inside your HTML using `find` and `find_all` functions
+
+```python
+#Looking any div element
+div_main_content = soup.find('div',id='main-content')
+
+#Looking any <li> element which its parent, grandparent, great-grandparent, etc is the div element (id = 'main-content')
+li_elements = div_main_content.find_all('li',class_='movie', recursive=True)
+
+for li_element in li_elements:
+    print(li_element.text)
+```
--- bookstore_web_scraping.py
+++ bookstore_web_scraping.py
@@ -1,44 +1,76 @@
+'''
+This Python file is made for scraping a "Book Store Website": a fictional web page which its main purpose is to practice web scrapping.
+
+In this case, I'm using the BeautifulSoup library to extract the HTML elements and extract the data. Finally, I'm exporting the data into a CSV file and reading it with pandas.
+
+Author: Luis Miguel Miranda
+Date: 2022-12-28
+'''
+
+# Importing libraries
 import requests
-import json
-from bs4 import BeautifulSoup
 import re
+from bs4 import BeautifulSoup
 import csv
 import pandas as pd
 
 
-def getting_list_of_books():
+def getting_book_catalogue():
+    """
+    Get the book catalogue
+    
+    Parameters:
+    * No parameter required *
+    
+    Returns:
+    list(String): The book catalogue
+    """
 
-    book_type_list = []
+    book_category_list = []
 
     url = "https://books.toscrape.com"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
+
     results = soup.find('div',class_="container-fluid page")
     div_sidebar = results.find('aside',class_="sidebar")
     list_books_li = div_sidebar.find_all('li')
-    for li_element in list_books_li:
-        book_type_a = li_element.find('a')
-        book_type_list.append(book_type_a.text.strip().lower())
-    
-    return book_type_list
 
-def scraping_bookstore_webpage(book_type_list):
+    for li_element in list_books_li:
+        book_category_a = li_element.find('a')
+        book_category_list.append(book_category_a.text.strip().lower())
     
+    return book_category_list
+
+def scraping_bookstore_webpage(book_category_list):
+    """
+    Scraping the website to obtain the basic info per each book.
+    1) Accessing the page with the list per each book category (url: ...../category/books/{book_category}/index.html)
+    2) If the book category has more than 20 books, it would have more than 1 html 
+    3) Inside the HTML for the list of 1 book category, we have a link to each book. I'm getting the HTML of each book too.
+    
+    Parameters:
+    * No parameter required *
+    
+    Returns:
+    list(String): The book catalogue
+    """
+
     with open('bookstore_scraping.csv', mode='w') as bookstore_file:
 
         bookstore_writer = csv.writer(bookstore_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-        book_header = ['Book Row','Book Name','Book URL','Book Type','Book Price','Book UPC','Book Availability','Books Available','Book Reviews','Book Rating']
+        book_header = ['Book Row','Book Name','Book URL','Book Category','Book Price','Book UPC','Book Availability','Books Available','Book Reviews','Book Rating']
 
         bookstore_writer.writerow(book_header)
 
         book_id = 1
 
-        for idx in range(1,len(book_type_list)):
+        for idx in range(1,len(book_category_list)):
             
-            book_type_for_url = str.replace(book_type_list[idx],' ','-')
+            book_category_for_url = str.replace(book_category_list[idx],' ','-')
             
-            url = f"https://books.toscrape.com/catalogue/category/books/{book_type_for_url}_{idx+1}/index.html"
+            url = f"https://books.toscrape.com/catalogue/category/books/{book_category_for_url}_{idx+1}/index.html"
             page = requests.get(url)
             soup = BeautifulSoup(page.content, "html.parser")
 
@@ -52,18 +84,15 @@ def scraping_bookstore_webpage(book_type_list):
             books_pager_ul = books_list_section.find('ul',class_="pager")
             if (books_pager_ul is None):
                 books_total_pages = 1
-                #print('is Empty')
             else:
                 books_pager_current = books_pager_ul.find('li',class_="current").text.strip()
                 books_total_pages = int(books_pager_current.split(' ')[3])
-                #print(books_total_pages)
 
             for idx_page in range(0,books_total_pages):
                 
-                if (books_total_pages == 1):
-                    url = f"https://books.toscrape.com/catalogue/category/books/{book_type_for_url}_{idx+1}/index.html"
-                else:
-                    url = f"https://books.toscrape.com/catalogue/category/books/{book_type_for_url}_{idx+1}/page-{idx_page+1}.html"
+                #If the book category contains more than 1 page, I'm changing the URL
+                if (books_total_pages != 1):
+                    url = f"https://books.toscrape.com/catalogue/category/books/{book_category_for_url}_{idx+1}/page-{idx_page+1}.html"
 
                 page = requests.get(url)
                 soup = BeautifulSoup(page.content, "html.parser")
@@ -126,7 +155,7 @@ def scraping_bookstore_webpage(book_type_list):
                     price_div = list_li_article.find('div',class_="product_price")
                     book_price = str.replace(price_div.find('p',class_="price_color").text,'£','')
                     
-                    book_element = [book_id, book_name, book_link,book_type_list[idx],book_price,book_upc,book_stock_availability,book_stock_value,book_review,rating_value]
+                    book_element = [book_id, book_name, book_link,book_category_list[idx],book_price,book_upc,book_stock_availability,book_stock_value,book_review,rating_value]
                     
                     book_id += 1
 
@@ -134,15 +163,42 @@ def scraping_bookstore_webpage(book_type_list):
 
         print('The Scraping Process on the Book Store website has been finished!')
 
-def getting_total_books_per_type():
+def getting_total_books_per_category():
+    """
+    Reading the csv file to a pandas' dataframe and making some aggregations
+    
+    Parameters:
+    * No parameter required *
+    
+    Returns:
+    * No return value required *
+    """
+
     df = pd.read_csv('bookstore_scraping.csv')
-    df_count = df[['Book Row','Book Type']].groupby(['Book Type']).count().sort_values(by=['Book Row'],ascending=False)
+    
+    df_count = df[['Book Row','Book Category','Book Price']]\
+                    .groupby(['Book Category'])\
+                    .agg({'Book Row':'count','Book Price':['sum','mean','min','max']})\
+                    .sort_values(by=[('Book Row','count')],ascending=False)
+    
+    #Keeping the top 10 (based on the Book count)
     df_count = df_count.head(10)
+
+    #Printing ranking of book categories
     print(df_count)
 
 def _init_():
-    book_type_list = getting_list_of_books()
-    scraping_bookstore_webpage(book_type_list)
-    getting_total_books_per_type()
+    """
+    Main function
+    
+    Parameters:
+    * No parameter required *
+    
+    Returns:
+    * No return value required *
+    """
+    book_category_list = getting_book_catalogue()
+    scraping_bookstore_webpage(book_category_list)
+    getting_total_books_per_category()
 
 _init_()
